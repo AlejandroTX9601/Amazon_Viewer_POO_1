c24a2a972ac62bf6505b576378b95d26792c84aa
RAI <RAI@RAI-PC>
2018-11-06 01:00:12 -0500

    Se Implemento la Salida Hola Mundo Rama1

--- src/com/pobjetos/controlador/Controlador.java
+++ src/com/pobjetos/controlador/Controlador.java
@@ -1,9 +1,13 @@
 package com.pobjetos.controlador;
 
+import javax.swing.JOptionPane;
+
 /**
   * @Instituto Rumiñahui
  * @author SSolis
  */
 public class Controlador {
-
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo Rama1");
+    }
 }
