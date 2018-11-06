1c212530061c02ad71938a690598aaa571a9f095
RAI <RAI@RAI-PC>
2018-11-06 01:21:29 -0500

    Bienvenidos Rama 4

--- src/com/pobjetos/controlador/Controlador.java
+++ src/com/pobjetos/controlador/Controlador.java
@@ -8,6 +8,6 @@ import javax.swing.JOptionPane;
  */
 public class Controlador {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Rama3");
+        JOptionPane.showMessageDialog(null, "Bienvenidos a Netbeans Rama4");
     }
 }
